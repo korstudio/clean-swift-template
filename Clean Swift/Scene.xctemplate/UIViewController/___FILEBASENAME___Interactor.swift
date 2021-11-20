77508d5fcce89ae1806d9cc7bededbb90e954d6c
Methas Tariya <korstudio@gmail.com>
2021-11-21 01:15:13 +0700

    Initial commit

---
+++ Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+protocol ___VARIABLE_sceneName___BusinessLogic {}
+
+protocol ___VARIABLE_sceneName___DataStore {}
+
+class ___VARIABLE_sceneName___Interactor: ___VARIABLE_sceneName___BusinessLogic, ___VARIABLE_sceneName___DataStore {
+    var presenter: ___VARIABLE_sceneName___PresentationLogic?
+    var worker: ___VARIABLE_sceneName___Worker?
+}
